85a1c8c285c706ea5c6e803b9bc31dd23b534176
Simon Weller <s.meierbeck@gmail.com>
2020-09-01 14:20:00 +0200

    Add basic booking form and bookings overview

--- src/main/kotlin/de/sweller/fewospring/BookingsController.kt
+++ src/main/kotlin/de/sweller/fewospring/BookingsController.kt
@@ -1,19 +1,42 @@
 package de.sweller.fewospring
 
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import java.time.LocalDate
+
+const val DATE_INPUT_FORMAT = "yyyy-MM-dd"
 
 @Controller
-@RequestMapping("/bookings")
+@RequestMapping
 class BookingsController {
 
-    @GetMapping
-    fun getBookingForm() = "bookings"
+    private var bookings = listOf<Booking>()
 
-    @PostMapping
-    fun createBooking() {
-        println("create booking")
+    @GetMapping("/booking-form")
+    fun getBookingForm() = "booking-form"
+
+    @GetMapping("/bookings")
+    fun getBookings(model: Model): String {
+        model.addAttribute("bookings", bookings)
+        return "bookings"
+    }
+
+    @PostMapping("/bookings")
+    fun createBooking(booking: Booking): String {
+        bookings = bookings + booking
+        return "redirect:/bookings"
     }
 }
+
+data class Booking(
+        val firstName: String,
+        val lastName: String,
+        @DateTimeFormat(pattern = DATE_INPUT_FORMAT)
+        val arrivalDate: LocalDate,
+        @DateTimeFormat(pattern = DATE_INPUT_FORMAT)
+        val departureDate: LocalDate,
+)
