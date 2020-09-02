0852eed449994693142f53fab69d297a0bd6b721
Simon Weller <s.meierbeck@gmail.com>
2020-09-02 14:09:02 +0200

    Send confirmation email

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
--- src/main/kotlin/de/sweller/fewospring/booking/BookingService.kt
+++ src/main/kotlin/de/sweller/fewospring/booking/BookingService.kt
@@ -1,14 +1,32 @@
 package de.sweller.fewospring.booking
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 
 @Service
 class BookingService(
     val bookingRepository: BookingRepository,
+    val mailSender: JavaMailSender,
 ) {
+
+    @Value("\${MAIL_USER}")
+    lateinit var fromAddress: String
+
     fun getBookings(): MutableIterable<Booking> = bookingRepository.findAll()
 
     fun addBooking(bookingData: BookingData) {
         bookingRepository.save(bookingData.toBooking())
+        sendConfirmationMail(bookingData.email)
+    }
+
+    private fun sendConfirmationMail(toAddress: String) {
+        val message = SimpleMailMessage()
+        message.setFrom(fromAddress)
+        message.setTo(toAddress)
+        message.setSubject("Confirmation mail")
+        message.setText("Your booking is hereby confirmed!")
+        mailSender.send(message)
     }
 }
