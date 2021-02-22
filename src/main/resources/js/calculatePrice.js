7262b48da57540f7bb0a210aead8f357ff78e025
Simon Weller <s.meierbeck@gmail.com>
2021-02-22 22:56:00 +0100

    Correctly calculate bungalow price

--- src/main/resources/js/calculatePrice.js
+++ src/main/resources/js/calculatePrice.js
@@ -1,16 +1,18 @@
 export function calculatePrice(numberOfNights, adults, children, extraBedroom, isApartment) {
 	const people = adults + children;
 	const effectiveNights = getEffectiveNights(numberOfNights, people);
-	const pricePerNight = getPricePerNight(numberOfNights, adults, children);
+	const pricePerNight = getPricePerNight(numberOfNights, adults, children, isApartment, extraBedroom);
 	return effectiveNights * pricePerNight;
 }
 
-function getPricePerNight(numberOfNights, adults, children) {
+function getPricePerNight(numberOfNights, adults, children, isApartment, extraBedroom) {
 	const people = adults + children;
-	const baseChargePerNight = getBaseChargePerNight(numberOfNights, people);
+	const baseChargePerNight = getBaseChargePerNight(numberOfNights, people, isApartment);
 	const additionalAdults = Math.max(adults - 2, 0);
 	const additionalChildren = adults === 1 ? Math.max(children - 1, 0) : children;
-	return baseChargePerNight + additionalAdults * 10 + (additionalChildren * 5);
+	const chargePerAdditionalAdult = isApartment ? 10 : 15;
+	const extraBedroomCharge = extraBedroom && people <= 3 ? 10 : 0;
+	return baseChargePerNight + (additionalAdults * chargePerAdditionalAdult) + (additionalChildren * 5) + extraBedroomCharge;
 }
 
 function getEffectiveNights(numberOfNights, people) {
@@ -18,7 +20,11 @@ function getEffectiveNights(numberOfNights, people) {
 	return Math.max(numberOfNights, 2);
 }
 
-function getBaseChargePerNight(numberOfNights, people) {
+function getBaseChargePerNight(numberOfNights, people, isApartment) {
 	if (people === 1) return 25;
-	return numberOfNights > 2 ? 40 : 50;
+	if (isApartment) {
+		return numberOfNights > 2 ? 40 : 50;
+	} else {
+		return numberOfNights > 2 ? 50 : 60;
+	}
 }
--- src/main/resources/js/calculatePrice.test.js
+++ src/main/resources/js/calculatePrice.test.js
@@ -41,4 +41,51 @@ describe(`calculatePrice`, () => {
 			expect(calculatePrice(1, 3, 0, false, true)).toBe(120);
 		});
 	});
+
+	describe(`bungalow`, () => {
+		it(`should use a base price of 60€ per night for 2 people`, () => {
+			expect(calculatePrice(2, 2, 0, false, false)).toBe(120);
+		});
+
+		it(`should use a base price of 50€ per night for 2 people staying 3 nights or more`, () => {
+			expect(calculatePrice(3, 2, 0, false, false)).toBe(150);
+			expect(calculatePrice(4, 2, 0, false, false)).toBe(200);
+		});
+
+		it(`should add 15€ per night for an additional person`, () => {
+			expect(calculatePrice(3, 3, 0, false, false)).toBe(195);
+			expect(calculatePrice(4, 3, 0, false, false)).toBe(260);
+
+			expect(calculatePrice(3, 4, 0, false, false)).toBe(240);
+			expect(calculatePrice(4, 4, 0, false, false)).toBe(320);
+
+			expect(calculatePrice(3, 5, 0, false, false)).toBe(285);
+			expect(calculatePrice(4, 5, 0, false, false)).toBe(380);
+		});
+
+		it(`should add 5€ per night for a child`, () => {
+			expect(calculatePrice(3, 2, 1, false, false)).toBe(165);
+			expect(calculatePrice(4, 2, 1, false, false)).toBe(220);
+		});
+
+		it(`should treat 1 adult and 1 child like 2 adults`, () => {
+			expect(calculatePrice(2, 1, 1, false, false)).toBe(120);
+			expect(calculatePrice(3, 1, 1, false, false)).toBe(150);
+		});
+
+		it(`should correctly enforce the minimum number of nights`, () => {
+			expect(calculatePrice(1, 2, 0, false, false)).toBe(120);
+			expect(calculatePrice(1, 1, 1, false, false)).toBe(120);
+		});
+
+		it(`should add 10€ per night for the extra bedroom for up to 3 people`, () => {
+			expect(calculatePrice(2, 2, 0, true, false)).toBe(140);
+			expect(calculatePrice(3, 3, 0, true, false)).toBe(225);
+		});
+
+		it(`should not add an extra charge for the extra bedroom for more than 3 people`, () => {
+			expect(calculatePrice(2, 2, 2, true, false)).toBe(140);
+			expect(calculatePrice(3, 4, 0, true, false)).toBe(240);
+		});
+	});
 });
