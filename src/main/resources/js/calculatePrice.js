152d89250907209ed36001ddc6f3c9559ba24f3c
Simon Weller <s.meierbeck@gmail.com>
2021-02-22 22:38:43 +0100

    Correctly calculate apartment price

--- src/main/resources/js/calculatePrice.js
+++ src/main/resources/js/calculatePrice.js
@@ -1,3 +1,24 @@
 export function calculatePrice(numberOfNights, adults, children, extraBedroom, isApartment) {
-	return 75;
+	const people = adults + children;
+	const effectiveNights = getEffectiveNights(numberOfNights, people);
+	const pricePerNight = getPricePerNight(numberOfNights, adults, children);
+	return effectiveNights * pricePerNight;
+}
+
+function getPricePerNight(numberOfNights, adults, children) {
+	const people = adults + children;
+	const baseChargePerNight = getBaseChargePerNight(numberOfNights, people);
+	const additionalAdults = Math.max(adults - 2, 0);
+	const additionalChildren = adults === 1 ? Math.max(children - 1, 0) : children;
+	return baseChargePerNight + additionalAdults * 10 + (additionalChildren * 5);
+}
+
+function getEffectiveNights(numberOfNights, people) {
+	if (people === 1) return Math.max(numberOfNights, 3);
+	return Math.max(numberOfNights, 2);
+}
+
+function getBaseChargePerNight(numberOfNights, people) {
+	if (people === 1) return 25;
+	return numberOfNights > 2 ? 40 : 50;
 }
--- src/main/resources/js/calculatePrice.test.js
+++ src/main/resources/js/calculatePrice.test.js
@@ -1,13 +1,44 @@
-import { calculatePrice } from './calculatePrice'
+import {calculatePrice} from './calculatePrice'
 
 describe(`calculatePrice`, () => {
-	it(`should return 75€ for 1 person staying in the apartment for 3 nights`, () => {
-		const numberOfNights = 3;
-		const adults = 1;
-		const children = 0;
-		const extraBedroom = false;
-		const isApartment = true;
+	describe(`apartment`, () => {
+		it(`should use a base price of 25€ per night for 1 person`, () => {
+			expect(calculatePrice(3, 1, 0, false, true)).toBe(75);
+			expect(calculatePrice(4, 1, 0, false, true)).toBe(100);
+		});
 
-		expect(calculatePrice(numberOfNights, adults, children, extraBedroom, isApartment)).toBe(75);
+		it(`should use a base price of 50€ per night for 2 people`, () => {
+			expect(calculatePrice(2, 2, 0, false, true)).toBe(100);
+		});
+
+		it(`should use a base price of 40€ per night for 2 people staying 3 nights or more`, () => {
+			expect(calculatePrice(3, 2, 0, false, true)).toBe(120);
+			expect(calculatePrice(4, 2, 0, false, true)).toBe(160);
+		});
+
+		it(`should add 10€ per night for an additional person`, () => {
+			expect(calculatePrice(3, 3, 0, false, true)).toBe(150);
+			expect(calculatePrice(4, 3, 0, false, true)).toBe(200);
+		});
+
+		it(`should add 5€ per night for a child`, () => {
+			expect(calculatePrice(3, 2, 1, false, true)).toBe(135);
+			expect(calculatePrice(4, 2, 1, false, true)).toBe(180);
+		});
+
+		it(`should treat 1 adult and 1 child like 2 adults`, () => {
+			expect(calculatePrice(2, 1, 1, false, true)).toBe(100);
+			expect(calculatePrice(3, 1, 1, false, true)).toBe(120);
+		});
+
+		it(`should correctly enforce the minimum number of nights`, () => {
+			expect(calculatePrice(1, 1, 0, false, true)).toBe(75);
+			expect(calculatePrice(2, 1, 0, false, true)).toBe(75);
+
+			expect(calculatePrice(1, 2, 0, false, true)).toBe(100);
+			expect(calculatePrice(1, 1, 1, false, true)).toBe(100);
+			expect(calculatePrice(1, 1, 2, false, true)).toBe(110);
+			expect(calculatePrice(1, 3, 0, false, true)).toBe(120);
+		});
 	});
 });
