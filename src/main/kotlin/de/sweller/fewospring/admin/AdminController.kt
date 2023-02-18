851c83b4a1b9ea50ea2571740a4922eb540c963c
Simon Weller <simon.weller@tooltime.de>
2023-02-18 14:23:03 +0100

    Sort bookings in descending order on admin page

--- src/main/kotlin/de/sweller/fewospring/admin/AdminController.kt
+++ src/main/kotlin/de/sweller/fewospring/admin/AdminController.kt
@@ -6,18 +6,22 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.server.ResponseStatusException
 
 @RequestMapping("/admin/bookings")
 @Controller
 class AdminController(
-        val bookingService: BookingService,
+    val bookingService: BookingService
 ) {
 
     @GetMapping
     fun getBookings(model: Model): String {
-        model.addAttribute("bookings", bookingService.getBookings().sortedBy { it.arrivalDate })
+        model.addAttribute("bookings", bookingService.getBookings().sortedByDescending { it.arrivalDate })
         return "admin"
     }
 
